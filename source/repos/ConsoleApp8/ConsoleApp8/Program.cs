215e60ab19d0ee673da4e8bacbc2f3e9ba9337f6
userblb <userblb@UNI002-L003-13.serlab.intra>
2018-03-28 11:26:42 -0300

    fibonacci adicionado

---
+++ source/repos/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+
+{
+    public static void Main(string[] args)
+    {
+
+        int numeroAnterior = 0;
+        int numeroAtual = 1;
+        int novoNumero;
+        int fibonacci;
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            fibonacci = numeroAnterior + numeroAtual;
+            Console.WriteLine(fibonacci);
+            numeroAnterior = numeroAtual;
+            numeroAtual = fibonacci;
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
+    
